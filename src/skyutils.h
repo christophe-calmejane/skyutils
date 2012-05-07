75d92c185bfd67a6252be808c5b8af2cf7d76a62
Kiki <dakikifr@free.fr>
2012-05-07 22:18:46 +0200

    Update version

--- src/skyutils.h
+++ src/skyutils.h
@@ -28,7 +28,7 @@
 #endif /* FD_SETSIZE */
 #define FD_SETSIZE 256
 
-#define SKYUTILS_VERSION "3.76"
+#define SKYUTILS_VERSION "3.77"
 #define SKYUTILS_AUTHOR "Christophe Calméjane"
 
 #if defined(__MACH__) || defined(_AIX)
